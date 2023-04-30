0473cf3032b11d367f8a97c42fb5af26d54a0986
轻字节编程(Light Byte Code) <111341725+LightByteCode@users.noreply.github.com>
2023-04-30 16:19:05 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,31 +2,36 @@
 #include <iostream>
 #include <easyx.h>
 #include <conio.h>
-#include <time.h>
+#include <ctime>
 #include <windows.h>
 #include <string.h>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
-void start()
-{
-	setbkcolor(BLACK);
-	cleardevice();
-	setlinecolor(WHITE);
-	line(0, 240, 640, 240);
-	line(320, 0, 320, 480);
-	for (int i = 0; i <= 640; i += 10)
+void linear_function(int k, int b) {
+	b = b * 10;
+	setlinecolor(GREEN);
+	int x, y; int xx, yy; int xxx, yyy;
 	{
-		for (int j = 0; j <= 480; j += 10)
+		x = -320;
+		y = k * x + b;
+		xxx = xx = x + 320;
+		yyy = yy = -y + 240;
+		line(xx, yy, xx, yy);
+		for (int x = -319; x <= 320; x++)
 		{
-			putpixel(i, j, LIGHTGRAY);
+			y = k * x + b;
+			xx = x + 320;
+			yy = -y + 240;
+			line(xx, yy, xxx, yyy);
+			xxx = x + 320;
+			yyy = -y + 240;
 		}
 	}
 }
 
-void two(int a, int b, int c)
-{
+void quadratic_function(int a, int b, int c) {
 	double a1 = double(a), b1 = double(b), c1 = double(c);
 	double x, y, xx, yy, xxx, yyy;
 	setlinecolor(YELLOW);
@@ -48,30 +53,8 @@ void two(int a, int b, int c)
 		}
 	}
 }
-void one(int k, int b)
-{
-	b = b * 10;
-	setlinecolor(GREEN);
-	int x, y; int xx, yy; int xxx, yyy;
-	{
-		x = -320;
-		y = k * x + b;
-		xxx = xx = x + 320;
-		yyy = yy = -y + 240;
-		line(xx, yy, xx, yy);
-		for (int x = -319; x <= 320; x++)
-		{
-			y = k * x + b;
-			xx = x + 320;
-			yy = -y + 240;
-			line(xx, yy, xxx, yyy);
-			xxx = x + 320;
-			yyy = -y + 240;
-		}
-	}
-}
-void minus_one(int k)
-{
+
+void inverse_proportional_function(int k) {
 	double k1 = double(k);
 	double x, y, xx, yy, xxx, yyy;
 	setlinecolor(LIGHTBLUE);
@@ -94,47 +77,22 @@ void minus_one(int k)
 	}
 }
 
-void input()
-{
-	char type;
-	int a, b, c;
-	int k;//b
-	//int k;
-	cout << "输入:\n2:y=ax^2+bx+c;\n:->y=kx+b;\n0:y=k/x\n";
-	cin >> type;
-	switch (type)
-	{
-	case '2':
-		cout << "y=ax^2+bx+c\n";
-		cout << "a=";
-		cin >> a;
-		cout << '\n';
-		cout << "b=";
-		cin >> b;
-		cout << '\n';
-		cout << "c=";
-		cin >> c;
-		cout << '\n';
-		//initgraph(640,480,SHOWCONSOLE);	
-		two(a, b, c); break;
-
-	case '1':
-		cout << "k=\n";
-		cin >> k;
-		cout << '\n';
-		cout << "b=\n";
-		cin >> b;
-		cout << '\n';
-		//initgraph(640,480,SHOWCONSOLE);
-		one(k, b);
-		break;
-	case '0':
-		cout << "k=\n";
-		cin >> k;
-		cout << '\n';
-		//initgraph(640,480,SHOWCONSOLE);	
-		minus_one(k);
-		break;
+void draw_canvas() {
+	//开始--绘制画布
+	setbkcolor(BLACK);
+	cleardevice();
+	setlinecolor(LIGHTCYAN);
+	line(0, 240, 640, 240);
+	line(320, 0, 320, 480);
+	for (int x = 0; x <= 640; x += 10){
+		for (int y = 0; y <= 480; y += 10){
+			if (x == 320 || y == 240) {
+				continue;
+			}
+			else {
+				putpixel(x, y, LIGHTGRAY);
+			}
+		}
 	}
 }
 
@@ -147,8 +105,45 @@ int main()
 		NOMINIMIZE   no 最小化
 		DBLCLKS      在绘图窗口中支持鼠标双击事件
 	*/
-	start();
-	input();
+	draw_canvas();
+	//定义变量
+	char type;
+	int a, b, c, k;
+	//输入
+	cout << "欢迎使用函数图像绘制工具！\n按下2:二次函数;\n按下1:一次函数;\n按下0:反比例函数\n按下3:退出(exit)\n按下4:清空图像\n\n";
+	while (true) {
+		type = _getch();//输入选择
+		if (type == '2') {
+			cout << "函数表达式:y=ax^2+bx+c\na=";
+			cin >> a;
+			cout << "b=";
+			cin >> b;
+			cout << "c=";
+			cin >> c;
+			quadratic_function(a, b, c);
+		}
+		else if (type == '1') {
+			cout << "函数表达式:y=kx+b\nk=";
+			cin >> k;
+			cout << "b=";
+			cin >> b;
+			linear_function(k, b);
+		}
+		else if (type == '0') {
+			cout << "k=";
+			cin >> k;
+			inverse_proportional_function(k);
+		}
+		else if (type == '3') {
+			return 0;
+		}
+		else if (type == '4') {
+			cleardevice();
+			draw_canvas();
+		}
+		cout << '\n';
+	}
+	//程序结束
 	_getwch();
 	closegraph();
 	return 0;
